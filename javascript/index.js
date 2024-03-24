b0500ca161d11cf23a1a0ecc0ee73405eb136008
10Sumi10 <sushma.gurung478@gmail.com>
2024-03-24 18:51:24 +0000

    Updated AM and PM

--- javascript/index.js
+++ javascript/index.js
@@ -38,7 +38,7 @@ function updateCity(event) {
 	<div class="date">${cityTime.format("MMMM Do YYYY")}</div>
 	</div>
 	<div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
-		"A"
+		" A "
 	)}</small></div>
 	</div>
     `;
